678936e45668b8a8884f39aecfff89e18338e108
blitz <blitzkreigkhan@gmail.com>
2026-01-29 20:01:14 -0800

    end coding for the day

--- microblog.py
+++ microblog.py
@@ -1,1 +1,8 @@
-from app_pack import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app_pack import app, db
+from app_pack.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
